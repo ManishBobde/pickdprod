d624923c303c861bc9de831c2d0188046cf4cbd0
Manish <manish@pickd.in>
2016-12-24 00:41:37 +0530

    Changes to database.php for adding pickdapp DB URl

--- config/database.php
+++ config/database.php
@@ -1,10 +1,19 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+$pickdurl = parse_url(getenv("CLEARDB_DATABASE_URL_PICKD"));
+
+
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
+
+
+$pickdhost = $pickdurl["host"];
+$pickdusername = $pickdurl["user"];
+$pickdpassword = $pickdurl["pass"];
+$pickddatabase = substr($pickdurl["path"], 1);
 return [
 
     /*
@@ -51,12 +60,6 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'host' => $host,
@@ -70,20 +73,20 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+ 		
+ 		'pickdmysql' => [
+            'driver' => 'mysql',
+            'host' => $pickdhost,
+            'port' => env('DB_PORT', '3306'),
+            'database' => $pickddatabase,
+            'username' => $pickdusername,
+            'password' => $pickdpassword,
             'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+            'strict' => true,
+            'engine' => null,
         ],
-
     ],
 
     /*
